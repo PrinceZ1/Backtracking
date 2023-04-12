6f39fbee8f4a4ebce5dc82f550214de5d7c75777
PrinceZ1 <88963977+PrinceZ1@users.noreply.github.com>
2023-04-12 11:42:34 +0700

    Update QuayLui_day_con_co_tong_bang_k.cpp

--- QuayLui_day_con_co_tong_bang_k.cpp
+++ QuayLui_day_con_co_tong_bang_k.cpp
@@ -1,4 +1,6 @@
-/*Cho dãy số A[] = (a1, a2, .., an) và số tự nhiên K. Hãy đưa ra tất cả các dãy con của dãy số A[] sao cho tổng các phần tử của dãy con đó đúng bằng K. Các phần tử của dãy số A[] được giả thuyết là nguyên dương và không có các phần tử giống nhau. Ví dụ với dãy con A[] = {5, 10, 15, 20, 25}, K = 50 ta có 3 dãy con {5, 10, 15, 20}, {5, 20, 25}, {10, 15, 25}.
+/*Cho dãy số A[] = (a1, a2, .., an) và số tự nhiên K. Hãy đưa ra tất cả các dãy con của dãy số A[] sao cho tổng các phần tử của dãy con đó đúng bằng K. 
+Các phần tử của dãy số A[] được giả thuyết là nguyên dương và không có các phần tử giống nhau. 
+Ví dụ với dãy con A[] = {5, 10, 15, 20, 25}, K = 50 ta có 3 dãy con {5, 10, 15, 20}, {5, 20, 25}, {10, 15, 25}.
 
 Input Format
 
@@ -10,7 +12,8 @@ Constraints
 
 Output Format
 
-Đưa ra tất cả các dãy con của dãy số A[] thỏa mãn yêu cầu bài toán theo thứ tự từ điển, trong đó mỗi dãy con được bao bởi các ký tự [, ]. Nếu không có dãy con nào thỏa mãn yêu cầu bài toán, hãy đưa ra -1.
+Đưa ra tất cả các dãy con của dãy số A[] thỏa mãn yêu cầu bài toán theo thứ tự từ điển, 
+trong đó mỗi dãy con được bao bởi các ký tự [, ]. Nếu không có dãy con nào thỏa mãn yêu cầu bài toán, hãy đưa ra -1.
 
 Sample Input 0
 
